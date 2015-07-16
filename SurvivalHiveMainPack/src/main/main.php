020140543cbd62194232e935cf292ec6cf8b0182
SurvivalHive <hell-death@gmx.de>
2015-07-16 13:38:19 +0200

    Create main.php

---
+++ SurvivalHiveMainPack/src/main/main.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace main;
+
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\IPlayer;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\Config;
+//---------------------------
+use main\afkkicker\afkkicker;
+use main\antidoublechest\antidoublechest;
+use main\allwaysonspawn\allwaysonspawn;
+use main\liftsign\liftsign;
+use main\mutejoin\mutejoin;
+use main\worldborder\worldborder;
+//---------------------------
