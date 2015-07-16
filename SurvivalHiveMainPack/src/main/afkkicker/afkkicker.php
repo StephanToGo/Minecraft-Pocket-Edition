d41fa82961d03a0dcbdf6856aba6ef1f43d030b6
SurvivalHive <hell-death@gmx.de>
2015-07-16 12:48:15 +0200

    Create afkkicker.php

---
+++ SurvivalHiveMainPack/src/main/afkkicker/afkkicker.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace main\afkkicker;
+
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\IPlayer;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\PluginTask;
+
+	class afkkicker extends PluginTask
+	{
+		public function __construct(Plugin $owner) 
+		{
+			parent::__construct($owner);
+		}
+	
+		public function onRun($currentTick)
+		{
+			//$this->getOwner()->getLogger()->info(MT::GREEN."AFKKICKER onRun");
+			foreach($this->getOwner()->getServer()->getOnlinePlayers() as $player)
+			{
+				$name = $player->getName();
+				$coords = (round($player->getX()).",".round($player->getY()).",".round($player->getZ()));
+	
+				if(!$player->isOp())
+				{
+					if(!(isset($this->getOwner()->player[$name]['Coords'])))$this->getOwner()->player[$name]['Coords'] = 0;
+					if(!(isset($this->getOwner()->player[$name]['Counter'])))$this->getOwner()->player[$name]['Counter'] = 0;
+					
+					if($this->getOwner()->player[$name]['Coords'] == $coords)
+					{	
+						if($this->getOwner()->player[$name]['Counter'] == 1) $player->kick($reason=MT::RED."KICK! -> A-F-K");
+						if($this->getOwner()->player[$name]['Counter'] == 0) $this->getOwner()->player[$name]['Counter'] = 1;
+					}
+					else
+					{
+						$this->getOwner()->player[$name]['Counter'] = 0;
+					}
+					$this->getOwner()->player[$name]['Coords'] = $coords;
+				}
+				$this->getOwner()->getLogger()->info(MT::YELLOW."AFKKICKER: $name ". $this->getOwner()->player[$name]['Counter'] ." ".$this->getOwner()->player[$name]['Coords'] );	
+			}
+			//$this->getOwner()->getLogger()->info(MT::GREEN."AFKKICKER onEnd");
+		}
+	}
