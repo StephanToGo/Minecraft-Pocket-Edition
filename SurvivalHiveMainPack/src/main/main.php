e11918215bf48e1217e10a9b2a74047b3a2b359f
SurvivalHive <hell-death@gmx.de>
2015-07-27 11:24:45 +0200

    Update main.php

--- SurvivalHiveMainPack/src/main/main.php
+++ SurvivalHiveMainPack/src/main/main.php
@@ -18,16 +18,24 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\Config;
 //---------------------------
 use main\afkkicker\afkkicker;
+
+use main\antifly\antifly;
+use main\antifly\antiflyevent;
+
 use main\antidoublechest\antidoublechest;
 use main\allwaysonspawn\allwaysonspawn;
 use main\liftsign\liftsign;
 use main\antipvp\antipvp;
 use main\antibuild\antibuild;
+use main\particle\particleplayer;
+use main\sounds\joinsound;
+
 use main\mutejoin\mutejoin;
 use main\bposition\bposition;
 use main\jumpsign\jumpsign;
 use main\sneaken\sneaken;
 use main\waffenkits\waffenkits;
+use main\weiterleitung\weiterleitung;
 use main\worldborder\worldborder;
 use main\vipslot\vipslot;
 //---------------------------
@@ -37,6 +45,20 @@ use main\commandhandler as commandhandler;
 	class main extends PluginBase implements Listener
 	
 	{
+		public $player = array();
+		public $schalter = array();
+		
+		public $antidoublechest;
+		//public $antifly;
+		public $allwaysonspawn;
+		public $mutejoin;
+		public $liftsign;
+		public $bposition;
+		
+		public $worldborder;
+		
+		private $command_class;
+		
 		public function onEnable()
 		{
 			$this->getLogger()->info(MT::GOLD."SurvivalHive Main loaded!");
@@ -47,20 +69,26 @@ use main\commandhandler as commandhandler;
 				@mkdir($this->getDataFolder(), true);
 			}
 			$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+					"Anleitung/Manuell" => "PlayerParticleTypen = Spirale/Herzen | JoinSoindTypen = BatSound/ClickSound/",
 					"debugmode" => true,
 					"AFK-Kick" => true,
 					"AntiBuildGenerell" => true,
 					"AntiPvPGenrell" => true,
 					"AntiDoubleChest" => true,
 					"AllwaysOnSpawn" => true,
+					"Antifly" => true,
 					"Liftsign" => true,
 					"MuteJoin" => true,
 					"JumpSign" => true,
 					"Waffenkits" => true,
 					"Sneaken" => true,
 					"BlockPosition" => true,
+					"PlayerParticle" => true,
+					"PlayerParticleTyp" => "Spirale",
+					"JoinSound" => true,
+					"JoinSoundTyp" => "BatSound",
 					"Weiterleitung" => false,
-					"WeiterleitungIP" => "0.0.0.0",
+					"WeiterleitungIP" => "148.251.4.154",
 					"WeiterleitungPort" => "19132",
 					"WorldBorder" => true,
 					"WorldBorderPos1" => "1,1,1",
