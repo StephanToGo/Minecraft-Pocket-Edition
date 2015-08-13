acf0a03edd6b0c6953dc40369a9cf3397ce6c19d
Stephan Dahmen <hell-death@gmx.de>
2015-08-13 11:48:58 +0200

    +

--- SurvivalHiveStats/src/main/stats.php
+++ SurvivalHiveStats/src/main/stats.php
@@ -7,6 +7,8 @@ use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\IPlayer;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 use pocketmine\block\Block;
@@ -19,19 +21,79 @@ use pocketmine\entity\Living;
 use pocketmine\event\block\SignChangeEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityMoveEvent;
 use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\inventory\InventoryCloseEvent;
+use pocketmine\event\inventory\InventoryPickupItemEvent;
+use pocketmine\event\player\PlayerAchievementAwardedEvent;
+use pocketmine\event\player\PlayerAnimationEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerGameModeChangeEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerKickEvent;
 use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\event\Timings;
+use pocketmine\inventory\BaseTransaction;
+use pocketmine\inventory\BigShapelessRecipe;
+use pocketmine\inventory\CraftingTransactionGroup;
+use pocketmine\inventory\FurnaceInventory;
+use pocketmine\inventory\Inventory;
+use pocketmine\inventory\InventoryHolder;
+use pocketmine\inventory\SimpleTransactionGroup;
+use pocketmine\inventory\StonecutterShapelessRecipe;
 use pocketmine\item\Item;
 use pocketmine\level\format\FullChunk;
 use pocketmine\level\format\LevelProvider;
 use pocketmine\level\Level;
 use pocketmine\level\Position;
+use pocketmine\math\Vector2;
+use pocketmine\math\Vector3;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\metadata\MetadataValue;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\Byte;
+use pocketmine\nbt\tag\Compound;
+use pocketmine\nbt\tag\Double;
+use pocketmine\nbt\tag\Enum;
+use pocketmine\nbt\tag\Float;
+use pocketmine\nbt\tag\Int;
+use pocketmine\nbt\tag\String;
+use pocketmine\network\protocol\AdventureSettingsPacket;
+use pocketmine\network\protocol\AnimatePacket;
+use pocketmine\network\protocol\DataPacket;
+use pocketmine\network\protocol\EntityEventPacket;
+use pocketmine\network\protocol\FullChunkDataPacket;
+use pocketmine\network\protocol\Info as ProtocolInfo;
+use pocketmine\network\protocol\LoginStatusPacket;
+use pocketmine\network\protocol\MessagePacket;
+use pocketmine\network\protocol\MovePlayerPacket;
+use pocketmine\network\protocol\SetHealthPacket;
+use pocketmine\network\protocol\SetSpawnPositionPacket;
+use pocketmine\network\protocol\SetTimePacket;
+use pocketmine\network\protocol\StartGamePacket;
+use pocketmine\network\protocol\TakeItemEntityPacket;
+use pocketmine\network\protocol\UpdateBlockPacket;
+use pocketmine\network\SourceInterface;
+use pocketmine\permission\PermissibleBase;
+use pocketmine\permission\PermissionAttachment;
 use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\CallbackTask;
+use pocketmine\tile\Sign;
+use pocketmine\tile\Spawnable;
+use pocketmine\tile\Tile;
+use pocketmine\utils\ReversePriorityQueue;
 use pocketmine\utils\TextFormat;
 
 class stats extends PluginBase implements Listener 
@@ -77,7 +139,20 @@ class stats extends PluginBase implements Listener
 		$this->columdeaths = $this->cfg->get("columdeaths");
 		$this->columkills = $this->cfg->get("columkills");
 		
+		$link = mysqli_connect("$this->serverip", "$this->user", "$this->password");
+		$sql = "CREATE DATABASE IF NOT EXISTS `$this->database`";
+		$db_selected = mysqli_query($link,$sql);
+		
 		$this->mysqli = mysqli_connect("$this->serverip", "$this->user", "$this->password", "$this->database");
+		
+		$sql2 = "CREATE TABLE IF NOT EXISTS `$this->tablename` (
+                          `$this->columname` varchar(255) NOT NULL,
+                          `$this->columkills` int(11) NOT NULL,
+                          `$this->columdeaths` int(11) NOT NULL,
+                          `$this->columjoins` int(11) NOT NULL,
+                          PRIMARY KEY  ($this->columname)
+                          )";
+		$db_selected = mysqli_query($this->mysqli,$sql2);
 	}
 	
 	public function onPlayerJoinEvent(PlayerJoinEvent $event)
@@ -156,14 +231,14 @@ class stats extends PluginBase implements Listener
 		}
 		if(strtolower($command->getName()) == "stats") 
 		{
-			$database = $this->database;
-			$tname = $this->tablename;
-			$join = $this->tablejoin;
-			$kill = $this->tablekill;
-			$death = $this->tabledeath;
+			$database = $this->tablename;
+			$tname = $this->columname;
+			$join = $this->columjoins;
+			$kill = $this->columkills;
+			$death = $this->columdeaths;
 			
 			$name = strtolower($sender->getName());
-  			$sql = "SELECT `$name`,`$join`,`$kill`,`$death` FROM `$database` WHERE `$tname` = `$name`";
+  			$sql = "SELECT `$tname`,`$join`,`$kill`,`$death` FROM `$database` WHERE `$tname` = '$name'";
   			$result = $this->mysqli->query($sql);
   			$row = mysqli_fetch_row($result);
   			$sender->sendMessage("Player: " . $row[0] . " Joins: " . $row[1] . " Kills: " . $row[2] . " Death: " . $row[3] );
