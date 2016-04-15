757cfcc1226fb977ba9390ddab69a5cdc4ba76db
SurvivalHive <hell-death@gmx.de>
2016-04-15 12:25:01 +0200

    add code

---
+++ SurvivalHiveKickBan(not finished)/src/main/kickban.php
@@ -0,0 +1,59 @@
+<?php
+namespace main;
+
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\server;
+use pocketmine\level;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\math\Vector3;
+
+	class kickban extends PluginBase implements Listener
+	
+	{
+		public function onEnable()
+		{
+			$this->getServer()->getPluginManager()->registerEvents($this,$this);
+			$this->getLogger()->info(MT::AQUA."-=SH=-KickBan loading...!");
+		}
+	
+		public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+		{
+			if($p instanceof Player)
+			{
+				if(strtolower($command->getName()) == "shban")
+				{
+					if(isset($args[1]))
+					{
+					
+					}
+					else
+					{
+						$sender->sendMessage(MT::RED."bitte Spielernamen eingeben");
+					}
+				}
+				if(strtolower($command->getName()) == "shkick")
+				{
+					if(isset($args[1]))
+					{
+						
+					}
+					else
+					{
+						$sender->sendMessage(MT::RED."bitte Spielernamen eingeben");
+					}
+				}
+			}
+		}
+			
+		public function onDisable()
+		{
+			$this->getLogger()->info(MT::AQUA."Plugin unloaded!");
+		}
+	}
