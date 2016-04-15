5173edcea4c415aad8a05bd0a028bcb82ba6cdbf
SurvivalHive <hell-death@gmx.de>
2016-04-15 12:33:03 +0200

    add functions

--- SurvivalHivePosition/src/main/pos.php
+++ SurvivalHivePosition/src/main/pos.php
@@ -2,39 +2,98 @@
 
 namespace main;
 
-use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
+use pocketmine\IPlayer;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as MT;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
 use pocketmine\utils\Config;
+use pocketmine\block\Block;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
 use pocketmine\math\Vector3;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\metadata\MetadataValue;
+use pocketmine\plugin\Plugin;
+use pocketmine\tile\Sign;
+use pocketmine\utils\TextFormat;
 
 class pos extends PluginBase implements Listener {
-
+	
+	public $schalter = array();
+	
 	public function onEnable() 
 	{	
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getLogger()->info(MT::AQUA."Plugin -=SH=-Position loading...!");
+		$this->getLogger()->info(MT::AQUA."-=SH=-Position loading...!");
     }
-
-	public function onCommand(CommandSender $p, Command $command, $label, array $args) 
+	
+    public function onCommand(CommandSender $p, Command $command, $label, array $args)
+    {
+    	if($p instanceof Player) 
+    	{
+    		if(strtolower($command->getName()) == "shpos")
+    		{
+    			$id = $p->getID();
+    			$name = strtolower($p->getName());
+    		
+    			if (! (in_array($id, $this->schalter)))
+    			{
+    				$this->schalter[$name] = $id;
+    				$p->sendMessage("Position Eingeschaltet");
+    				return true;
+    			}
+    			else
+    			{
+    				$index = array_search($id, $this->schalter);
+    				unset($this->schalter[$index]);
+    				$p->sendMessage("Position Ausgeschaltet");
+    				return true;
+    			}
+    		}
+    	}
+    	else 
+    	{
+    	$p->sendMessage("Nur im Spiel moeglich");
+    	return true;
+    	}
+    	break;
+    }
+    
+	public function onBlockBreak(BlockBreakEvent $event)
 	{
-		if(strtolower($command->getName()) == "shpos") 
+		$id = $event->getPlayer()->getID();
+		
+		if(in_array($id, $this->schalter))
 		{
-			if($p instanceof Player) 
-			{
-				$n = strtolower($p->getName());
-				$this->pos[$n] = new Vector3(round($p->getX()),round($p->getY()),round($p->getZ()));
-				$p->sendMessage(MT::AQUA."Position(" . $this->pos[$n]->getX() . "," . $this->pos[$n]->getY() . "," . $this->pos[$n]->getZ() . ")");
-			}
+			$bl = $event->getBlock();
+			$n = strtolower($event->getPlayer()->getName());
+			$this->pos[$n] = new Vector3($bl->getX(),$bl->getY(),$bl->getZ());
+			$event->getPlayer()->sendMessage("Position(" . $this->pos[$n]->getX() . "," . $this->pos[$n]->getY() . "," . $this->pos[$n]->getZ() . ")");
+			$event->setCancelled(true);
+		}
+	}
+	
+	public function onBlockPlace(BlockPlaceEvent $event)
+	{
+		$id = $event->getPlayer()->getID();
+	
+		if(in_array($id, $this->schalter))
+		{
+			$bl = $event->getBlock();
+			$n = strtolower($event->getPlayer()->getName());
+			$this->pos[$n] = new Vector3($bl->getX(),$bl->getY(),$bl->getZ());
+			$event->getPlayer()->sendMessage("Position(" . $this->pos[$n]->getX() . "," . $this->pos[$n]->getY() . "," . $this->pos[$n]->getZ() . ")");
+			$event->setCancelled(true);
 		}
 	}
 	
 	public function onDisable()
 	{
-		$this->getLogger()->info(MT::AQUA."Plugin unloaded!");			
+		$this->getLogger()->info(MT::AQUA."Plugin unloaded!");
 	}
 }
