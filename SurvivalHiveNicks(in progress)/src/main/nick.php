c935a1d6df89e4a76e33384b461e0181718d3f83
SurvivalHive <hell-death@gmx.de>
2016-04-20 10:13:22 +0200

    add code

---
+++ SurvivalHiveNicks(in progress)/src/main/nick.php
@@ -0,0 +1,81 @@
+<?php
+namespace main;
+
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\event\player\PlayerInteractEvent;
+
+	class nick extends PluginBase implements Listener
+	{
+		public $schalter = array();
+		public $namesave = array();
+		
+		public function onEnable()
+		{
+			$this->getServer()->getPluginManager()->registerEvents($this,$this);
+			$this->getLogger()->info(MT::AQUA."Plugin -=SH=-Nicks loading...!");
+
+			if (!file_exists($this->getDataFolder())){@mkdir($this->getDataFolder(), true);}
+			$this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML, array("Nicknames" => []));
+			
+			$this->nicks = $this->config->get("Nicknames");
+		}
+		
+		public function onDisable()
+		{
+			$this->getLogger()->info(MT::AQUA."Plugin unloaded!");			
+		}
+
+		public function onCommand(CommandSender $p, Command $command, $label, array $args)
+		{
+			if($p instanceof Player) 
+			{
+				if(strtolower($command->getName()) == "shnick")
+				{
+					$id = $p->getID();
+					$name = strtolower($p->getName());
+				
+					if (! (in_array($id, $this->schalter)))
+					{
+						$this->schalter[$name] = $id;
+						$p->sendMessage(MT::GREEN."SHNick Eingeschaltet");
+						return true;
+					}
+					else
+					{
+						$index = array_search($id, $this->schalter);
+						unset($this->schalter[$index]);
+						$p->sendMessage(MT::GREEN."SHNick Ausgeschaltet");
+						return true;
+					}
+				}
+			}
+			else 
+			{
+				$p->sendMessage("Nur im Spiel moeglich");
+				return true;
+			}
+		}
+		public function playerBlockTouch(PlayerInteractEvent $event)
+		{
+			$id = $event->getPlayer()->getID();
+		
+			if(in_array($id, $this->schalter))
+			{
+				if(!isset($this->namesave[$id])){$this->namesave[$id] = $event->getPlayer()->getName();}
+				$anzahldernicks = count($this->nicks)-1;
+				$rand = mt_rand(0, $anzahldernicks)
+				$randnickname = $this->nicks[$rand];
+				$event->getPlayer()->sendMessage("$randnickname");
+				$event->getPlayer()->setNameTag("$randnickname");
+			}
+		}
