3d56e9fdcfe74d9ca27fef8f6210e97b729b3292
SurvivalHive <hell-death@gmx.de>
2016-04-21 12:26:56 +0200

    add touch permissions

--- SurvivalHiveLiftSign/src/main/lift.php
+++ SurvivalHiveLiftSign/src/main/lift.php
@@ -66,42 +66,70 @@ class lift extends PluginBase implements Listener
 
     public function playerBlockTouch(PlayerInteractEvent $event)
     {
-        if($event->getBlock()->getID() == 323 || $event->getBlock()->getID() == 63 || $event->getBlock()->getID() == 68)
+		$bl = $event->getBlock();
+		$p = $event->getPlayer();
+		
+        if($bl->getID() == 323 || $bl->getID() == 63 || $bl->getID() == 68)
         {
-           $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
-           
+			$sign = $p->getLevel()->getTile($bl);
             if(!($sign instanceof Sign)){return;}
-            
             $sign = $sign->getText();
-            
-            if($sign[0] == '[Lift hoch]')
-            {
-            	for ($i = 1; $i <= 32; $i++) {
-            		$bl = $event->getBlock();
-            		$pos = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x,$bl->y+$i,$bl->z));
-            		if($pos->getID() == 323 || $pos->getID() == 63 || $pos->getID() == 323)
-            		{
-            			$event->getPlayer()->teleport(new Position($bl->x,$bl->y+$i,$bl->z));
-            			return true;
-            		}
-            	}
-            	
-            }
-            if($sign[0] == '[Lift runter]')
-            {
-            	for($i = 32; $i >=0; $i--) {
-            		$bl = $event->getBlock();
-            		$pos = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x,$bl->y-$i,$bl->z));
-            		if($pos->getID() == 323 || $pos->getID() == 63 || $pos->getID() == 323)
-            		{
-            			$event->getPlayer()->teleport(new Position($bl->x,$bl->y-$i,$bl->z));
-            			return true;
-            		}
-            	}
-            	 
-            }
+			
+			if($this->permissions == true)
+			{
+				if($p->isOp() || $p->hasPermission('survivalhive.nicks'))
+				{
+					if($sign[0] == '[Lift hoch]')
+					{
+						$this->onLift($bl, $p, 'hoch');	
+					}
+					if($sign[0] == '[Lift runter]')
+					{
+						$this->onLift($bl, $p, 'runter');
+					}
+				}
+			}
+			else
+			{
+				if($sign[0] == '[Lift hoch]')
+				{
+					$this->onLift($bl, $p, 'hoch');	
+				}
+				if($sign[0] == '[Lift runter]')
+				{
+					$this->onLift($bl, $p, 'runter');
+				}
+			}
         }
     }
+	
+	public function onLift($bl, $p, $var)
+	{
+		if($var == 'hoch')
+		{
+			for ($i = 1; $i <= 32; $i++) 
+			{
+				$pos = $bl->getLevel()->getBlock(new Vector3($bl->x,$bl->y+$i,$bl->z));
+				if($pos->getID() == 323 || $pos->getID() == 63 || $pos->getID() == 323)
+				{
+					$p->teleport(new Position($bl->x,$bl->y+$i,$bl->z));
+					return true;
+				}
+			}
+		}
+		if($var == 'runter')
+		{
+			for($i = 32; $i >=0; $i--) 
+			{
+            	$pos = $bl->getLevel()->getBlock(new Vector3($bl->x,$bl->y-$i,$bl->z));
+            	if($pos->getID() == 323 || $pos->getID() == 63 || $pos->getID() == 323)
+            	{
+            		$p->teleport(new Position($bl->x,$bl->y-$i,$bl->z));
+            		return true;
+            	}
+            }
+		}
+	}
     
     public function onDisable()
     {
