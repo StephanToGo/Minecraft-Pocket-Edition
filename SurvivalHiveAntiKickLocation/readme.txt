b89f3624e8870143fb6175b9fb9d3572edb612ac
SurvivalHive <hell-death@gmx.de>
2016-05-19 09:07:49 +0200

    add plugin

---
+++ SurvivalHiveAntiKickLocation/readme.txt
@@ -0,0 +1,5 @@
+This Plugin stop kicking the player if another one
+will connect with the same name...
+
+Das Plugin unterbindet das der Spieler gekickt wird
+sollte ein anderer mit gleichen namen versuchen zu joinen.
---
+++ SurvivalHiveAntiKickLocation/src/main/kickloc.php
@@ -0,0 +1,41 @@
+<?php 
+namespace main;
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\level;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\math\Vector3;
+use pocketmine\event\server\QueryRegenerateEvent;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+
+class kickloc extends PluginBase implements Listener
+{	
+		public function onEnable()
+		{
+			$this->getServer()->getPluginManager()->registerEvents($this,$this);
+			$this->getLogger()->info(MT::AQUA."Plugin -=SH=-AntiKickLoc loading...!");
+		}
+		
+		public function onKick(PlayerKickEvent $event)
+		{
+			$grund = $event->getReason();
+			$kicklocationreason = 'logged in from another location';
+
+			if($grund = $kicklocationreason)
+			{
+				$event->setCancelled();
+			}
+			return;
+		}
+		
+		public function onDisable(){
+			$this->getLogger()->info(MT::AQUA."Plugin unloaded!");					
+		}
+}
