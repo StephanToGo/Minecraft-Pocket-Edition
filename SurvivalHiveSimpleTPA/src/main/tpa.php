ae4375f4c705295fb83797c6d1f81e0bcb83d063
JackboyPlay <johannes.leutgeb2002@gmail.com>
2016-05-28 11:22:16 +0200

    Update des Codes
    Tolles update xD

--- SurvivalHiveSimpleTPA/src/main/tpa.php
+++ SurvivalHiveSimpleTPA/src/main/tpa.php
@@ -25,6 +25,8 @@ use pocketmine\utils\TextFormat as MT;
 
 class tpa extends PluginBase implements Listener 
 {
+	public $tpa = array();
+	
 	public function onEnable()
 	{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -34,86 +36,59 @@ class tpa extends PluginBase implements Listener
 	public function onJoin(PlayerJoinEvent $event)
 	{
 		$name = $event->getPlayer()->getName();
-		$this->tpa[$name] = null;
+		unset($this->tpa[array_search($this->tpa, $name)]);
 	}
 	
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args) 
-	{
-		if($sender instanceof Player)
-		{
-			if(strtolower($command->getName()) == "tpa")
-			{
-				if(isset($args[0]))
-				{
-					$name = strtolower($sender->getPlayer()->getName());
-					
-					if($target = $sender->getServer()->getPlayer($args[0]))
-					{
-						$targetname = strtolower($sender->getServer()->getPlayer($args[0])->getName());
-						
-						$sender->getServer()->getPlayer($args[0])->sendMessage(MT::GREEN."$name hat Ihnen eine TP anfrage gesendet | $name has sent you a tpa request");
-						$sender->getServer()->getPlayer($args[0])->sendMessage(MT::YELLOW."/tpja zum bestaetigen /tpnein zum ablehnen | /tpja for accept /tpnein for decline");
-						
-						$sender->sendMessage(MT::GREEN."Tpa Anfrage wurde gesendet! || Tpa request has been sent!");
-						
-						$this->tpa[$targetname] = $name;
-						return true;
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		if($sender instanceof Player){
+			if(strtolower($command->getName()) == "tpa"){
+				if(!isset($args[0])){
+					$sender->sendMessage(MT::GREEN . "/tpa [Spielername]");
+				}
+				if(isset($args[0])){
+					$spieler = $this->getServer()->getPlayer($args[0]);
+					if($spieler == null){
+						$sender->sendMessage(MT::RED . "Der Spieler ist nicht online");
 					}
-					else
-					{
-						$sender->sendMessage(MT::RED."Spieler exestiert nicht in dieser Welt || Player dont exist in this world");
-						return true;
+					if($spieler !== null && $spieler->isOnline()){//Es reicht auch nur eine der 2 Optionen
+						$sender->sendMessage(MT::GREEN . "Teleport Anfrage an " . MT::RED . $spieler->getName() . MT::GREEN . " gesendet!");
+						$this->tpa[$spieler->getName()] = $sender->getName();
 					}
 				}
-				else
-				{
-					$sender->sendMessage(MT::RED."Bitte Spieler Namen angeben || Missing player name");
-					return true;
+			}
+			if(strtolower($command->getName()) == "tpja"){
+				if(!isset($args[0])){
+					$sender->sendMessage(MT::RED . "/tpja [Spieler]");
+				}
+				if(isset($args[0])){
+					$spieler = $this->getServer()->getPlayer($args[0]);
+					if($this->tpa[$spieler->getName()] == $sender->getName()){
+						$sender->sendMessage(MT::GREEN . "Du wurdest zu " . MT::RED . $spieler->getName() . MT::GREEN . " teleportiert!");
+						$spieler->sendMessage(MT::RED . $sender->getName() . MT::GREEN . " hat die Anfrage akzeptiert!");
+						$sender->teleport(new Position($spieler->getX(), $spieler->getY(), $spieler->getZ()));
+					}
+					if($this->tpa[$spieler->getName()] !== $sender->getName()){
+						$sender->sendMessage(MT::RED . "Der Spieler hat dir keine Anfrage geschickt!");
+					}
 				}
 			}
-			
-			if(strtolower($command->getName()) == "tpja")
-			{
-				$name = strtolower($sender->getName());
-				
-				if($this->tpa[$name] != null)
-				{
-					$ziel = $this->tpa[$name];
-					$target = $sender->getServer()->getPlayer($ziel);
-					
-					$pos = new Position($sender->x, $sender->y, $sender->z, $sender->getLevel());
-					$target->teleport($pos);
-					
-					$sender->getServer()->getPlayer($ziel)->sendMessage(MT::GREEN."Tpa Anfrage bestaetigt | Tpa request accepted");
-					$sender->sendMessage(MT::GREEN."Tpa Anfrage bestaetigt | Tpa request accepted");
-					
-					$this->tpa[$name] = null;
-					return true;
+			if(strtolower($command->getName()) == "tpnein"){
+				$spieler = $this->getServer()->getPlayer($args[0]);
+				if(!isset($args[0])){
+					$sender->sendMessage(MT::RED . "/tpnein [Spieler]");
 				}
-				return true;
-			}
-			
-			if(strtolower($command->getName()) == "tpnein")
-			{
-				$name = strtolower($sender->getName());
-			
-				if($this->tpa[$name] != null)
-				{
-				
-					$ziel = $this->tpa[$name];
+				if(isset($args[0])){
+					if($this->tpa[$spieler->getName()] == $sender->getName()){
+						$sender->sendMessage(MT::GREEN . "Du hast die Anfrage von " . MT::RED . $spieler->getName() . MT::GREEN . " abgelehnt!");
+						unset($this->tpa[array_search($this->tpa, $spieler->getName())]);
+						$spieler->sendMessage(MT::RED . $sender->getName() . MT::GREEN . " hat die ANfrage abgelehnt!");
+					}
+					if($this->tpa[$spieler->getName()] !== $sender->getName()){
+						$sender->sendMessage(MT::RED . "Der Spieler hat dir keine Anfrage geschickt!");
+					}
 					
-					$sender->getServer()->getPlayer($ziel)->sendMessage(MT::RED."Tpa Anfrage abgelehnt | Tpa request declined");
-					$sender->sendMessage("Tpa Anfrage abgelehnt | Tpa request declined");
-					
-					$this->tpa[$name] = null;
-					return true;
-				}	
+				}
 			}
-		}
-		else
-		{
-			$sender->sendMessage("Nur im Spiel moeglich | Only in game");
-			return true;
 		}
 	}
    
