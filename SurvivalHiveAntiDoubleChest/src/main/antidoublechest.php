86079cf1ebc07cf62d70c840d7000852d5537585
JackboyPlay <johannes.leutgeb2002@gmail.com>
2016-08-29 22:02:30 +0200

    Verbessert by JäckboiPläi
    Tolles apdäte

--- SurvivalHiveAntiDoubleChest/src/main/antidoublechest.php
+++ SurvivalHiveAntiDoubleChest/src/main/antidoublechest.php
@@ -1,5 +1,6 @@
 <?php
 namespace main;
+
 use pocketmine\utils\TextFormat as MT;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
@@ -13,37 +14,26 @@ use pocketmine\command\Command;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\math\Vector3;
 
-	class antidoublechest extends PluginBase implements Listener
-	
-	{
-		public function onEnable()
-		{
+	class antidoublechest extends PluginBase implements Listener{
+		public function onEnable(){
 			$this->getServer()->getPluginManager()->registerEvents($this,$this);
-			$this->getLogger()->info(MT::AQUA."-=SH=-AntiDoubleChest loading...!");
+			$this->getLogger()->info(MT::AQUA . "-=SH=-AntiDoubleChest loading...!");
 		}
-	
-		public function onPlayerPlaceBlock(BlockPlaceEvent $event)
-		{
+		public function onPlayerPlaceBlock(BlockPlaceEvent $event){
 			$blockID = $event->getBlock()->getID();
-			$bl = $event->getBlock();
-			$pos = $event->getBlock(new Vector3($bl->x,$bl->y,$bl->z));
-		
-			if ($blockID == 54)
-			{				
-				$block1 = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x+1,$bl->y,$bl->z));
-				$block2 = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x-1,$bl->y,$bl->z));
-				$block3 = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x,$bl->y,$bl->z+1));
-				$block4 = $event->getBlock()->getLevel()->getBlock(new Vector3($bl->x,$bl->y,$bl->z-1));
-						
-				if($block1->getID() == 54 || $block2->getID() == 54 || $block3->getID() == 54 || $block4->getID() == 54)
-				{
-					$event->setCancelled(true);
+			$bl = $event->getBlock()->getLevel();
+			if ($blockID == 54){				
+				$block1 = $bl->getBlock(new Vector3($bl->x+1,$bl->y,$bl->z));
+				$block2 = $bl->getBlock(new Vector3($bl->x-1,$bl->y,$bl->z));
+				$block3 = $bl->getBlock(new Vector3($bl->x,$bl->y,$bl->z+1));
+				$block4 = $bl->getBlock(new Vector3($bl->x,$bl->y,$bl->z-1));
+				if($block1->getID() == 54 || $block2->getID() == 54 || $block3->getID() == 54 || $block4->getID() == 54){
+					$event->setCancelled();
 				}
 			}
 		}
 		
-		public function onDisable()
-		{
-			$this->getLogger()->info(MT::AQUA."Plugin unloaded!");
+		public function onDisable(){
+			$this->getLogger()->info(MT::AQUA . "Plugin unloaded!");
 		}
 	}
